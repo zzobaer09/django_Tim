92b5f248d45fd8dad70922434db9fa756f44aa2b
Zobaer <zzobaer09@gmail.com>
2022-10-12 19:21:54 +0600

    create.html , forms.py,views.py and urls.py edited for form page

--- myFirstDjango/main/views.py
+++ myFirstDjango/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse , HttpResponseRedirect
 from .models import ToDoList , Item 
-
+from .forms import CreateNewList
 # Create your views here.
 
 
@@ -15,3 +15,18 @@ def index(response , id):
 def home(res):
 	return HttpResponse(render(res , "main/home.html", {}))	
 
+
+
+def create(response):
+	
+	if response.method == "POST":
+		form = CreateNewList(response.POST)
+		if form.is_valid():
+			name = form.cleaned_data["name"]
+			t = ToDoList(name=name)
+			t.save()
+
+			return HttpResponseRedirect("/%i"  %t.id)
+	else:
+		form = CreateNewList()
+	return HttpResponse(render(response , "main/create.html", {"form":form}))
