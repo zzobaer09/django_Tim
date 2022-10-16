2b9510c93f9bae0229a02e4ae80cb84758963ac3
Zobaer <zzobaer09@gmail.com>
2022-10-16 21:21:18 +0600

    in list.html templet and views.py i have made some modifications to add item to the database and can chackmerk if it is complete or not

--- myFirstDjango/main/views.py
+++ myFirstDjango/main/views.py
@@ -9,6 +9,28 @@ from .forms import CreateNewList
 def index(response , id):
 	t = ToDoList
 	ls = t.objects.get(id=id)
+
+	if response.method == "POST":
+
+		if response.POST.get("save"): 
+
+			for item in ls.item_set.all():
+				if response.POST.get("c"+str(item.id)) == "checked":
+					item.complete = True 
+				else:
+					item.complete = False
+
+				item.save()
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("newItemField")
+
+			if len(txt) > 2:
+				ls.item_set.create(text=txt , complete=False)
+
+
+
+
+
 	return HttpResponse(render(response , "main/list.html",{"ls":ls}))
 
 
@@ -21,7 +43,7 @@ def create(response):
 	
 	if response.method == "POST":
 		form = CreateNewList(response.POST)
-		if form.is_valid():
+		if form.is_valid(): 
 			name = form.cleaned_data["name"]
 			t = ToDoList(name=name)
 			t.save()
